399e504531a52517990dbef7813c2b4d8c13c463
nikhil <nikhil.s@srmtechsol.com>
2023-01-01 21:21:06 +0530

    commit4

--- project/wwwroot/js/UserRegi.js
+++ project/wwwroot/js/UserRegi.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(document).ready(function () {
+    $('input[type=text]').on('blur', function () {
+        $(this).each(function () {
+            nama($('#form3Example1m').val());
+            no($('#form3Example1n').val());
+        });
+    });
+
+
+    function nama(myname) {
+        if (!myname) {
+            $('<span>name cannot be empty</span>').insertAfter('#form3Example1m');
+        }
+    }
+
+
+    function no(mynomor) {
+        if (!mynomor) {
+            $('<span>number cannot be empty</span>').insertAfter('#form3Example1n');
+        }
+    }
+});
